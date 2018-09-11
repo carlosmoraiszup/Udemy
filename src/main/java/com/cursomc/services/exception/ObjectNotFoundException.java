313d0af3980de1f1f4e2951bf17dd8d7eb25cf65
Carlos César <carlos.morais@zup.com.br>
2018-09-11 14:58:54 -0300

    Ajustes no endpoint /categorias{id}

---
+++ src/main/java/com/cursomc/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.cursomc.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+
+    public ObjectNotFoundException(String msg, Throwable cause){
+        super(msg, cause);
+    }
+
+
+}
